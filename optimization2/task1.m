8c3d9ade0784fd55beb5762f4d39556222b2be96
jszum <jaroslaw.szumega@gmail.com>
2017-05-31 02:47:24 +0200

    Unconstrained: adding 3d contour plot.

--- optimization2/task1.m
+++ optimization2/task1.m
@@ -1,6 +1,12 @@
-pkg load symbolic
-#[x1,x2] = meshgrid(-10:0.01:10);
-syms x1 x2
-f = @(x1,x2) 100.*(x2 - x1.^2).^2 + (1 - x1).^2;
+%pkg load symbolic
+%#[x1,x2] = meshgrid(-10:0.01:10);
+%syms x1 x2
+%f = 100.*(x2 - x1.^2).^2 + (1 - x1).^2;
+%fs = @(x1,x2) 100.*(x2 - x1.^2).^2 + (1 - x1).^2;
+%
+%ezcontour(fs,[-3,3])
+%
 
-ezcontour(f,[-3,3])
+[X1, X2] = meshgrid(-2:0.01:2);
+fun = 100.*(X2 - X1.^2).^2 + (1 - X1).^2;
+contour3(X1,X2,fun,200)
