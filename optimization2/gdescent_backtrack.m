b047995f26b045c1660b8818201d03a45d702fef
jszum <jaroslaw.szumega@gmail.com>
2017-06-06 03:39:59 +0200

    Unconstrained: Task5 finished.

--- optimization2/gdescent_backtrack.m
+++ optimization2/gdescent_backtrack.m
@@ -6,7 +6,7 @@ c = 0.0001;
 df = 1;
 xk = x0;
 k = 1;
-  while ((norm(df) > err) && k != 10)
+  while ((norm(df) > err))
       [f,df] = fun(x0);
       a = backtracking(fun,x0,-df, rho, c);
       x0 = x0 - a*df;
